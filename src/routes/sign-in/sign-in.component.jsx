eca07708291ab55a76157474a57f62e2161511cc
Reed Whetstone <rwhetstone0934@gmail.com>
2022-06-23 12:48:14 -0600

    Litterally black magic submit form setup w/ react
    whatamidoing.jpg

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,24 @@
 /** @format */
 
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import SignInComponent from '../../components/sign-up-form/sign-up-form.component';
+
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
+  const logGooglePopupUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGooglePopupUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };

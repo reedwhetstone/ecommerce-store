cd2676cdb99f3cdb3452c1b0c3b87089050a4bfd
Reed Whetstone <rwhetstone0934@gmail.com>
2022-07-04 14:07:20 -0600

    Added cart context and shopping icon
    the cart now toggles open and closed onClick.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 import '../src/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +17,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>

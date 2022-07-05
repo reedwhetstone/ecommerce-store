b019d464e00bf3e37611b4f2d2e0fdedef34f315
Reed Whetstone <rwhetstone0934@gmail.com>
2022-07-05 12:35:49 -0600

    Added functionality to addToCart using CartContext
    can now add items to cart and see the product name and quantity in the
    dropdown.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,23 @@
 /** @format */
 
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import './cart-dropdown.styles.scss';
 
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <img />
-        <span></span>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
       <Button>CHECKOUT</Button>
     </div>

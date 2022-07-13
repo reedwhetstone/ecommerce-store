3108630013424ce3903913bbbd33fb08ba46846a
Reed Whetstone <rwhetstone0934@gmail.com>
2022-07-13 11:43:40 -0600

    Finished styling each category page
    fixed some styling errors.

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,28 @@
+/** @format */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './directory-item.styles.scss';
+
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="directory-body-container">
+        <Link to={`shop/${title}`}>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default DirectoryItem;
